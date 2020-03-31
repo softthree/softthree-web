a1356d04a12ed711153d7e57b44b829dabffbba4
ayaz <ayazhussainbs@gmail.com>
2020-04-01 00:19:07 +0500

    First commit

---
+++ js/active.js
@@ -0,0 +1,223 @@
+(function ($) {
+    'use strict';
+
+    var browserWindow = $(window);
+
+    var form = $('#ajax-contact');
+
+    $(form).submit(function (event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+
+        var formData = $(form).serializeArray();
+        let jsonData = {
+            to: 'softthree3@gmail.com'
+        };
+        formData.map(x => {
+            jsonData[x.name] = x.value;
+        })
+        console.log(formData, jsonData)
+        $.ajax({
+            type: 'POST',
+            contentType: 'application/json',
+            url: 'https://softthree.herokuapp.com/api/contact-us',
+            data: JSON.stringify(jsonData),
+            dataType: 'json',
+            success: function (data) {
+                console.log('Submission was successful.');
+                console.log(data);
+            },
+            error: function (data) {
+                console.log('An error occurred.');
+                console.log(data);
+            },
+        })
+    });
+
+    // :: 1.0 Preloader Active Code
+    browserWindow.on('load', function () {
+        $('.preloader').fadeOut('slow', function () {
+            $(this).remove();
+        });
+    });
+
+    browserWindow.on('beforeunload', function () {
+        browserWindow.scrollTop(0);
+    });
+
+    // :: 2.0 Nav Active Code
+    if ($.fn.classyNav) {
+        $('#pixelNav').classyNav();
+    }
+
+    // :: 3.0 Sliders Active Code
+    if ($.fn.owlCarousel) {
+        var welcomeSlide = $('.hero-slideshow');
+        var testiSlide = $('.testimonial-slides');
+
+        welcomeSlide.owlCarousel({
+            items: 1,
+            loop: true,
+            nav: false,
+            navText: ['Prev', 'Next'],
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 500,
+            animateIn: 'fadeIn',
+            animateOut: 'fadeOut'
+        });
+
+        welcomeSlide.on('translate.owl.carousel', function () {
+            var slideLayer = $("[data-animation]");
+            slideLayer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).removeClass('animated ' + anim_name).css('opacity', '0');
+            });
+        });
+
+        welcomeSlide.on('translated.owl.carousel', function () {
+            var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
+            slideLayer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).addClass('animated ' + anim_name).css('opacity', '1');
+            });
+        });
+
+        $("[data-delay]").each(function () {
+            var anim_del = $(this).data('delay');
+            $(this).css('animation-delay', anim_del);
+        });
+
+        $("[data-duration]").each(function () {
+            var anim_dur = $(this).data('duration');
+            $(this).css('animation-duration', anim_dur);
+        });
+
+        testiSlide.owlCarousel({
+            items: 1,
+            loop: true,
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 10000,
+            smartSpeed: 500
+        });
+    }
+
+    // :: 4.0 Masonary Gallery Active Code
+    if ($.fn.imagesLoaded) {
+        $('.pixel-portfolio').imagesLoaded(function () {
+            // filter items on button click
+            $('.portfolio-menu').on('click', 'button', function () {
+                var filterValue = $(this).attr('data-filter');
+                $grid.isotope({
+                    filter: filterValue
+                });
+            });
+            // init Isotope
+            var $grid = $('.pixel-portfolio').isotope({
+                itemSelector: '.single_gallery_item',
+                percentPosition: true,
+                masonry: {
+                    columnWidth: '.single_gallery_item'
+                }
+            });
+        });
+    }
+
+    // :: 5.0 Gallery Menu Style Active Code
+    $('.portfolio-menu button.btn').on('click', function () {
+        $('.portfolio-menu button.btn').removeClass('active');
+        $(this).addClass('active');
+    })
+
+    // :: 6.0 Magnific Popup Active Code
+    if ($.fn.magnificPopup) {
+        $('.zoom-img').magnificPopup({
+            type: 'image'
+        });
+    }
+    // :: 7.0 ScrollUp Active Code
+    if ($.fn.scrollUp) {
+        browserWindow.scrollUp({
+            scrollSpeed: 1500,
+            scrollText: '<i class="fa fa-angle-up"></i> Top'
+        });
+    }
+
+    // :: 8.0 CounterUp Active Code
+    if ($.fn.counterUp) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    }
+
+    // :: 9.0 Progress Bar Active Code
+    if ($.fn.circleProgress) {
+        $('#c1').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#28c5a0',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+    if ($.fn.circleProgress) {
+        $('#c2').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#28c5a0',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+    if ($.fn.circleProgress) {
+        $('#c3').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#28c5a0',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+    if ($.fn.circleProgress) {
+        $('#c4').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#28c5a0',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+
+    // :: 10.0 Tooltip Active Code
+    if ($.fn.tooltip) {
+        $('[data-toggle="tooltip"]').tooltip();
+    }
+
+    // :: 11.0 Prevent Default a Click
+    $('a[href="#"]').on('click', function ($) {
+        $.preventDefault();
+    });
+
+    // :: 12.0 Jarallax Active Code
+    if ($.fn.jarallax) {
+        $('.jarallax').jarallax({
+            speed: 0.2
+        });
+    }
+
+    // :: 13.0 Sticky Active Code
+    if ($.fn.sticky) {
+        $("#sticker").sticky({
+            topSpacing: 0
+        });
+    }
+
+    // :: 14.0 Wow Active Code
+    if (browserWindow.width() > 767) {
+        new WOW().init();
+    }
+
+})(jQuery);
